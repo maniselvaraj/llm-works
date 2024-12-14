85387ea076c6a1875aa4f8565c5608211d60bd83
Mani Selvaraj <mani.selvaraj07@gmail.com>
2024-12-14 12:24:39 -0600

    fetch from github and create better code

--- codegen/code_analysis_main.py
+++ codegen/code_analysis_main.py
@@ -3,15 +3,11 @@ import os
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 
-from langchain_community.document_loaders import GithubFileLoader
-
+from github_loader import fetch_java_files
 
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
-
 open_ai_key = os.getenv("OPENAI_API_KEY")
-#print(open_ai_key)
-
 warnings.filterwarnings("ignore")
 
 
@@ -22,7 +18,7 @@ def simple_openai_call():
     print(answer)
 
 
-def advanced_openai_call():
+def advanced_openai_call(source_code):
     llm = ChatOpenAI(api_key=os.environ["OPENAI_API_KEY"])
     prompt = ChatPromptTemplate.from_messages([
         ("system", "you are a helpful ai based code generator"),
@@ -31,22 +27,21 @@ def advanced_openai_call():
         ("user", "{input_java_class}"),
     ])
     chain = prompt | llm
-    chain_result = chain.invoke({"input_java_class": "test"})
-    print(chain_result)
-
-def load_java_class():
-   #print(os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN"))
-    loader = GithubFileLoader(
-        repo="maniselvaraj/springboot-demo",  # the repo name
-        branch="main",  # the branch name
-        access_token=os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN"),
-        github_api_url="https://api.github.com",
-        file_filter=lambda file_path: file_path.endswith(
-            ".java"
-        ),  # load all markdowns files.
-    )
-    documents = loader.load()
-    print(documents)
+    chain_result = chain.invoke({"input_java_class": source_code})
+    return chain_result
 
 
-load_java_class()
+def main():
+    #get list of java files
+    java_files = fetch_java_files('src/main')
+
+    #iterate through each file and hit LLM
+    for java_file in java_files:
+        print("Processing file:" + java_file['file_source'])
+        source_code = java_file['page_content'].split('\n')[1:-1]
+        improved_code = advanced_openai_call(source_code)
+        print('=' * 80)
+        print(improved_code)
+        print('*' * 80)
+
+main()
